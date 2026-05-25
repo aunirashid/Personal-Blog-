cb988bd494074661b61ef8674f98f598c707c846
aunirashid <aunirashid04@gmail.com>
2026-05-25 16:52:48 +0800

    Updated homepage design

--- js/script.js
+++ js/script.js
@@ -11,3 +11,27 @@ function openDiary() {
     window.location.href = "about.html";
   }, 1000);
 }
+
+const music = document.getElementById("bgMusic");
+const musicPlayer = document.querySelector(".music-player");
+const musicText = document.getElementById("musicText");
+
+function toggleMusic() {
+  if (music.paused) {
+    music.play();
+    musicPlayer.classList.add("playing");
+    musicText.textContent = "♫ Playing";
+  } else {
+    music.pause();
+    musicPlayer.classList.remove("playing");
+    musicText.textContent = "🎧 Soft Music";
+  }
+}
+
+function showMessage() {
+  alert("This is a sample blog post preview.");
+}
+
+function sendMessage() {
+  alert("Thank you! Your message has been received.");
+}
